27974f77bfe2ca3b625512adc29a32ff35b2b664
littlemesie <littlemesie@163.com>
2019-01-18 20:09:23 +0800

    tf

--- tf_recommend/movie_recommend.py
+++ tf_recommend/movie_recommend.py
@@ -1,15 +1,78 @@
 # coding=utf-8
+import re
 import pandas as pd
 import numpy as np
 import tensorflow as tf
 
 # 数据预处理
 def load_data():
-    users_title = ['UserID', 'Gender', 'Age', 'OccupationID', 'Zip-code']
-    users = pd.read_table('../data/users.dat', sep='::', header=None, names=users_title, engine='python')
-    movies_title = ['MovieID', 'Title', 'Genres']
-    movies = pd.read_table('../data/movies.dat', sep='::', header=None, names=movies_title, engine='python')
+    pass
+
+
     ratings_title = ['UserID','MovieID', 'Rating', 'timestamps']
     ratings = pd.read_table('../data/ratings.dat', sep='::', header=None, names=ratings_title, engine='python')
 
-    print(ratings.head())
+def users_data():
+    users_title = ['UserID', 'Gender', 'Age', 'OccupationID', 'Zip-code']
+    users = pd.read_table('../data/users.dat', sep='::', header=None, names=users_title, engine='python')
+    users = users.filter(regex='UserID|Gender|Age|JobID')
+    # 没有做数据处理的原始用户数据
+    users_orig = users.values
+    # 改变User数据中性别和年龄
+    gender_map = {'F': 0, 'M': 1}
+    users['Gender'] = users['Gender'].map(gender_map)
+    age_map = {val: ii for ii, val in enumerate(set(users['Age']))}
+    users['Age'] = users['Age'].map(age_map)
+
+    return users, users_orig
+
+def movies_data():
+    movies_title = ['MovieID', 'Title', 'Genres']
+    movies = pd.read_table('../data/movies.dat', sep='::', header=None, names=movies_title, engine='python')
+    movies_orig = movies.values
+    # 将Title中的年份去掉
+    pattern = re.compile(r'^(.*)\((\d+)\)$')
+    title_map = {val: pattern.match(val).group(1) for ii, val in enumerate(set(movies['Title']))}
+    movies['Title'] = movies['Title'].map(title_map)
+
+    # 电影类型转数字字典
+    genres_set = set()
+    for val in movies['Genres'].str.split('|'):
+        genres_set.update(val)
+
+    genres_set.add('<PAD>')
+    genres2int = {val: ii for ii, val in enumerate(genres_set)}
+
+    # 将电影类型转成等长数字列表，长度是18
+    genres_map = {val: [genres2int[row] for row in val.split('|')] for ii, val in enumerate(set(movies['Genres']))}
+
+    for key in genres_map:
+        for cnt in range(max(genres2int.values()) - len(genres_map[key])):
+            genres_map[key].insert(len(genres_map[key]) + cnt, genres2int['<PAD>'])
+
+    movies['Genres'] = movies['Genres'].map(genres_map)
+
+    # 电影Title转数字字典
+    title_set = set()
+    for val in movies['Title'].str.split():
+        title_set.update(val)
+
+    title_set.add('<PAD>')
+    title2int = {val: ii for ii, val in enumerate(title_set)}
+
+    # 将电影Title转成等长数字列表，长度是15
+    title_count = 15
+    title_map = {val: [title2int[row] for row in val.split()] for ii, val in enumerate(set(movies['Title']))}
+
+    for key in title_map:
+        for cnt in range(title_count - len(title_map[key])):
+            title_map[key].insert(len(title_map[key]) + cnt, title2int['<PAD>'])
+
+    movies['Title'] = movies['Title'].map(title_map)
+
+
+    return movies, movies_orig
+
+
+if __name__ == '__main__':
+    movies_data()
