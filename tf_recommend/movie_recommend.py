fcc8d360e3f7467b716968371b2b537885a49792
littlemesie <ranjw@akulaku.com>
2019-01-31 18:16:08 +0800

    update

--- tf_recommend/movie_recommend.py
+++ tf_recommend/movie_recommend.py
@@ -1,78 +1,177 @@
 # coding=utf-8
-import re
-import pandas as pd
+import os
+import pickle
+import time
+import datetime
 import numpy as np
+from sklearn.model_selection import train_test_split
 import tensorflow as tf
 
-# 数据预处理
-def load_data():
-    pass
+def save_params(params):
+    """
+    Save parameters to file
+    """
+    pickle.dump(params, open('params.p', 'wb'))
 
 
-    ratings_title = ['UserID','MovieID', 'Rating', 'timestamps']
-    ratings = pd.read_table('../data/ratings.dat', sep='::', header=None, names=ratings_title, engine='python')
+def load_params():
+    """
+    Load parameters from file
+    """
+    return pickle.load(open('params.p', mode='rb'))
 
-def users_data():
-    users_title = ['UserID', 'Gender', 'Age', 'OccupationID', 'Zip-code']
-    users = pd.read_table('../data/users.dat', sep='::', header=None, names=users_title, engine='python')
-    users = users.filter(regex='UserID|Gender|Age|JobID')
-    # 没有做数据处理的原始用户数据
-    users_orig = users.values
-    # 改变User数据中性别和年龄
-    gender_map = {'F': 0, 'M': 1}
-    users['Gender'] = users['Gender'].map(gender_map)
-    age_map = {val: ii for ii, val in enumerate(set(users['Age']))}
-    users['Age'] = users['Age'].map(age_map)
+title_count, title_set, genres2int, features, targets_values, ratings, users, movies, data, movies_orig, users_orig = pickle.load(open('preprocess.p', mode='rb'))
 
-    return users, users_orig
+save_dir = './save'
 
-def movies_data():
-    movies_title = ['MovieID', 'Title', 'Genres']
-    movies = pd.read_table('../data/movies.dat', sep='::', header=None, names=movies_title, engine='python')
-    movies_orig = movies.values
-    # 将Title中的年份去掉
-    pattern = re.compile(r'^(.*)\((\d+)\)$')
-    title_map = {val: pattern.match(val).group(1) for ii, val in enumerate(set(movies['Title']))}
-    movies['Title'] = movies['Title'].map(title_map)
+save_params((save_dir))
 
-    # 电影类型转数字字典
-    genres_set = set()
-    for val in movies['Genres'].str.split('|'):
-        genres_set.update(val)
+load_dir = load_params()
 
-    genres_set.add('<PAD>')
-    genres2int = {val: ii for ii, val in enumerate(genres_set)}
+#电影ID转下标的字典，数据集中电影ID跟下标不一致，比如第5行的数据电影ID不一定是5
+movieid2idx = {val[0]: i for i, val in enumerate(movies.values)}
 
-    # 将电影类型转成等长数字列表，长度是18
-    genres_map = {val: [genres2int[row] for row in val.split('|')] for ii, val in enumerate(set(movies['Genres']))}
+#电影名长度
+sentences_size = title_count  # = 15
 
-    for key in genres_map:
-        for cnt in range(max(genres2int.values()) - len(genres_map[key])):
-            genres_map[key].insert(len(genres_map[key]) + cnt, genres2int['<PAD>'])
+def get_tensors(loaded_graph):
 
-    movies['Genres'] = movies['Genres'].map(genres_map)
-
-    # 电影Title转数字字典
-    title_set = set()
-    for val in movies['Title'].str.split():
-        title_set.update(val)
-
-    title_set.add('<PAD>')
-    title2int = {val: ii for ii, val in enumerate(title_set)}
-
-    # 将电影Title转成等长数字列表，长度是15
-    title_count = 15
-    title_map = {val: [title2int[row] for row in val.split()] for ii, val in enumerate(set(movies['Title']))}
-
-    for key in title_map:
-        for cnt in range(title_count - len(title_map[key])):
-            title_map[key].insert(len(title_map[key]) + cnt, title2int['<PAD>'])
-
-    movies['Title'] = movies['Title'].map(title_map)
+    uid = loaded_graph.get_tensor_by_name("uid:0")
+    user_gender = loaded_graph.get_tensor_by_name("user_gender:0")
+    user_age = loaded_graph.get_tensor_by_name("user_age:0")
+    user_job = loaded_graph.get_tensor_by_name("user_job:0")
+    movie_id = loaded_graph.get_tensor_by_name("movie_id:0")
+    movie_categories = loaded_graph.get_tensor_by_name("movie_categories:0")
+    movie_titles = loaded_graph.get_tensor_by_name("movie_titles:0")
+    targets = loaded_graph.get_tensor_by_name("targets:0")
+    dropout_keep_prob = loaded_graph.get_tensor_by_name("dropout_keep_prob:0")
+    lr = loaded_graph.get_tensor_by_name("LearningRate:0")
+    #两种不同计算预测评分的方案使用不同的name获取tensor inference
+#     inference = loaded_graph.get_tensor_by_name("inference/inference/BiasAdd:0")
+    inference = loaded_graph.get_tensor_by_name("inference/ExpandDims:0")
+    movie_combine_layer_flat = loaded_graph.get_tensor_by_name("movie_fc/Reshape:0")
+    user_combine_layer_flat = loaded_graph.get_tensor_by_name("user_fc/Reshape:0")
+    return uid, user_gender, user_age, user_job, movie_id, movie_categories, movie_titles, targets, lr, dropout_keep_prob, \
+           inference, movie_combine_layer_flat, user_combine_layer_flat
 
 
-    return movies, movies_orig
+def rating_movie(user_id_val, movie_id_val):
+    loaded_graph = tf.Graph()  #
+    with tf.Session(graph=loaded_graph) as sess:  #
+        # Load saved model
+        loader = tf.train.import_meta_graph(load_dir + '.meta')
+        loader.restore(sess, load_dir)
+
+        # Get Tensors from loaded model
+        uid, user_gender, user_age, user_job, movie_id, movie_categories, movie_titles, targets, lr, dropout_keep_prob, inference, _, __ = get_tensors(
+            loaded_graph)  # loaded_graph
+
+        categories = np.zeros([1, 18])
+        categories[0] = movies.values[movieid2idx[movie_id_val]][2]
+
+        titles = np.zeros([1, sentences_size])
+        titles[0] = movies.values[movieid2idx[movie_id_val]][1]
+
+        feed = {
+            uid: np.reshape(users.values[user_id_val - 1][0], [1, 1]),
+            user_gender: np.reshape(users.values[user_id_val - 1][1], [1, 1]),
+            user_age: np.reshape(users.values[user_id_val - 1][2], [1, 1]),
+            user_job: np.reshape(users.values[user_id_val - 1][3], [1, 1]),
+            movie_id: np.reshape(movies.values[movieid2idx[movie_id_val]][0], [1, 1]),
+            movie_categories: categories,  # x.take(6,1)
+            movie_titles: titles,  # x.take(5,1)
+            dropout_keep_prob: 1}
+
+        # Get Prediction
+        inference_val = sess.run([inference], feed)
+
+        return (inference_val)
+
+loaded_graph = tf.Graph()  #
+movie_matrics = []
+with tf.Session(graph=loaded_graph) as sess:  #
+    # Load saved model
+    loader = tf.train.import_meta_graph(load_dir + '.meta')
+    loader.restore(sess, load_dir)
+
+    # Get Tensors from loaded model
+    uid, user_gender, user_age, user_job, movie_id, movie_categories, movie_titles, targets, lr, dropout_keep_prob, _, movie_combine_layer_flat, __ = get_tensors(loaded_graph)  #loaded_graph
+
+    for item in movies.values:
+        categories = np.zeros([1, 18])
+        categories[0] = item.take(2)
+
+        titles = np.zeros([1, sentences_size])
+        titles[0] = item.take(1)
+
+        feed = {
+            movie_id: np.reshape(item.take(0), [1, 1]),
+            movie_categories: categories,  #x.take(6,1)
+            movie_titles: titles,  #x.take(5,1)
+            dropout_keep_prob: 1}
+
+        movie_combine_layer_flat_val = sess.run([movie_combine_layer_flat], feed)
+        movie_matrics.append(movie_combine_layer_flat_val)
+
+pickle.dump((np.array(movie_matrics).reshape(-1, 200)), open('movie_matrics.p', 'wb'))
+movie_matrics = pickle.load(open('movie_matrics.p', mode='rb'))
+
+loaded_graph = tf.Graph()  #
+users_matrics = []
+with tf.Session(graph=loaded_graph) as sess:  #
+    # Load saved model
+    loader = tf.train.import_meta_graph(load_dir + '.meta')
+    loader.restore(sess, load_dir)
+
+    # Get Tensors from loaded model
+    uid, user_gender, user_age, user_job, movie_id, movie_categories, movie_titles, targets, lr, dropout_keep_prob, _, __,user_combine_layer_flat = get_tensors(loaded_graph)  #loaded_graph
+
+    for item in users.values:
+
+        feed = {
+            uid: np.reshape(item.take(0), [1, 1]),
+            user_gender: np.reshape(item.take(1), [1, 1]),
+            user_age: np.reshape(item.take(2), [1, 1]),
+            user_job: np.reshape(item.take(3), [1, 1]),
+            dropout_keep_prob: 1}
+
+        user_combine_layer_flat_val = sess.run([user_combine_layer_flat], feed)
+        users_matrics.append(user_combine_layer_flat_val)
+
+pickle.dump((np.array(users_matrics).reshape(-1, 200)), open('users_matrics.p', 'wb'))
+users_matrics = pickle.load(open('users_matrics.p', mode='rb'))
 
 
-if __name__ == '__main__':
-    movies_data()
+def recommend_same_type_movie(movie_id_val, top_k=20):
+    loaded_graph = tf.Graph()  #
+    with tf.Session(graph=loaded_graph) as sess:  #
+        # Load saved model
+        loader = tf.train.import_meta_graph(load_dir + '.meta')
+        loader.restore(sess, load_dir)
+
+        norm_movie_matrics = tf.sqrt(tf.reduce_sum(tf.square(movie_matrics), 1, keep_dims=True))
+        normalized_movie_matrics = movie_matrics / norm_movie_matrics
+
+        # 推荐同类型的电影
+        probs_embeddings = (movie_matrics[movieid2idx[movie_id_val]]).reshape([1, 200])
+        probs_similarity = tf.matmul(probs_embeddings, tf.transpose(normalized_movie_matrics))
+        sim = (probs_similarity.eval())
+        #     results = (-sim[0]).argsort()[0:top_k]
+        #     print(results)
+
+        print("您看的电影是：{}".format(movies_orig[movieid2idx[movie_id_val]]))
+        print("以下是给您的推荐：")
+        p = np.squeeze(sim)
+        p[np.argsort(p)[:-top_k]] = 0
+        p = p / np.sum(p)
+        results = set()
+        while len(results) != 5:
+            c = np.random.choice(3883, 1, p=p)[0]
+            results.add(c)
+        for val in (results):
+            print(val)
+            print(movies_orig[val])
+
+        return results
+
+recommend_same_type_movie(1401, 20)
