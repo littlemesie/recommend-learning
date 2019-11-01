2117636cd2d5774daafd1289de5b89a09538d713
littlemesie <littlemesie@163.com>
2019-11-01 16:12:58 +0800

    update

--- src/fm/fm.py
+++ src/fm/fm.py
@@ -1,7 +1,6 @@
 from itertools import count
 from collections import defaultdict
 from scipy.sparse import csr
-import numpy as np
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction import DictVectorizer
@@ -15,27 +14,28 @@ def vectorize_dic(dic, ix=None, p=None, n=0, g=0):
     ix -- index generator (default None)
     p -- dimension of feature space (number of columns in the sparse matrix) (default None)
     """
-    if ix==None:
+    if ix == None:
         ix = dict()
 
     nz = n * g
 
-    col_ix = np.empty(nz,dtype = int)
+    col_ix = np.empty(nz, dtype=int)
 
     i = 0
     for k, lis in dic.items():
         for t in range(len(lis)):
-            ix[str(lis[t]) + str(k)] = ix.get(str(lis[t]) + str(k),0) + 1
+            ix[str(lis[t]) + str(k)] = ix.get(str(lis[t]) + str(k), 0) + 1
             col_ix[i+t*g] = ix[str(lis[t]) + str(k)]
         i += 1
 
-    row_ix = np.repeat(np.arange(0,n),g)
+    row_ix = np.repeat(np.arange(0, n), g)
     data = np.ones(nz)
     if p == None:
         p = len(ix)
-
+    # print(p)
     ixx = np.where(col_ix < p)
-    return csr.csr_matrix((data[ixx],(row_ix[ixx],col_ix[ixx])),shape=(n,p)),ix
+    # print(ixx)
+    return csr.csr_matrix((data[ixx],  (row_ix[ixx], col_ix[ixx])), shape=(n, p)), ix
 
 
 def batcher(X_, y_=None, batch_size=-1):
@@ -67,57 +67,57 @@ x_test, ix = vectorize_dic({'users': test['user'].values,
                            'items': test['item'].values}, ix, x_train.shape[1], n=len(test.index), g=2)
 
 
-print(x_train)
+# print(x_train)
+# print(ix)
 y_train = train['rating'].values
 y_test = test['rating'].values
-
+#
 x_train = x_train.todense()
 x_test = x_test.todense()
 
-print(x_train)
-
-print(x_train.shape)
-print(x_test.shape)
+# print(x_train)
+# print(x_train.shape)
+# print(x_test.shape)
 
 
 n, p = x_train.shape
 
 k = 10
 
-x = tf.placeholder('float',[None,p])
+x = tf.placeholder('float', [None, p])
 
-y = tf.placeholder('float',[None,1])
+y = tf.placeholder('float', [None, 1])
 
 w0 = tf.Variable(tf.zeros([1]))
 w = tf.Variable(tf.zeros([p]))
 
-v = tf.Variable(tf.random_normal([k,p],mean=0,stddev=0.01))
+v = tf.Variable(tf.random_normal([k, p], mean=0, stddev=0.01))
 
 #y_hat = tf.Variable(tf.zeros([n,1]))
 
-linear_terms = tf.add(w0,tf.reduce_sum(tf.multiply(w,x),1,keep_dims=True)) # n * 1
+linear_terms = tf.add(w0, tf.reduce_sum(tf.multiply(w, x), 1, keepdims=True))  # n * 1
 pair_interactions = 0.5 * tf.reduce_sum(
     tf.subtract(
         tf.pow(
-            tf.matmul(x,tf.transpose(v)),2),
-        tf.matmul(tf.pow(x,2),tf.transpose(tf.pow(v,2)))
-    ),axis = 1 , keep_dims=True)
+            tf.matmul(x, tf.transpose(v)), 2),
+        tf.matmul(tf.pow(x, 2), tf.transpose(tf.pow(v, 2)))
+    ), axis=1, keepdims=True)
 
 
-y_hat = tf.add(linear_terms,pair_interactions)
+y_hat = tf.add(linear_terms, pair_interactions)
 
-lambda_w = tf.constant(0.001,name='lambda_w')
-lambda_v = tf.constant(0.001,name='lambda_v')
+lambda_w = tf.constant(0.001, name='lambda_w')
+lambda_v = tf.constant(0.001, name='lambda_v')
 
 l2_norm = tf.reduce_sum(
     tf.add(
-        tf.multiply(lambda_w,tf.pow(w,2)),
-        tf.multiply(lambda_v,tf.pow(v,2))
+        tf.multiply(lambda_w, tf.pow(w, 2)),
+        tf.multiply(lambda_v, tf.pow(v, 2))
     )
 )
 
 error = tf.reduce_mean(tf.square(y-y_hat))
-loss = tf.add(error,l2_norm)
+loss = tf.add(error, l2_norm)
 
 
 train_op = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(loss)
@@ -131,11 +131,11 @@ init = tf.global_variables_initializer()
 with tf.Session() as sess:
     sess.run(init)
 
-    for epoch in tqdm(range(epochs), unit='epoch'):
+    for epoch in range(epochs):
         perm = np.random.permutation(x_train.shape[0])
         # iterate over batches
         for bX, bY in batcher(x_train[perm], y_train[perm], batch_size):
-            _,t = sess.run([train_op,loss], feed_dict={x: bX.reshape(-1, p), y: bY.reshape(-1, 1)})
+            _, t = sess.run([train_op, loss], feed_dict={x: bX.reshape(-1, p), y: bY.reshape(-1, 1)})
             print(t)
 
 
