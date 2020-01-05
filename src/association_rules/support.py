4404e5502e546d0268c5fe9695cfec6f4bf3640f
littlemesie <littlemesie@163.com>
2020-01-05 20:13:08 +0800

    update

--- src/association_rules/support.py
+++ src/association_rules/support.py
@@ -1,6 +1,28 @@
 
 def load_data():
     """加载数据"""
-    with open("")
+    data = []
+    with open("buy.txt", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            arr = list(map(int, line.replace("\n", "").split(',')))
+            data.append(arr)
+    return data
+
 def calc_support():
     """计算support"""
+    data = load_data()
+    sum = len(data)
+
+    itmes = dict()
+
+    for order in data:
+        for item in order:
+            itmes.setdefault(item, dict())
+
+
+
+
+if __name__ == '__main__':
+    data = load_data()
+    print(data)
