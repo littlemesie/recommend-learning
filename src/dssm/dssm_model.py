604b32f54708c36ed39265872a4d821c75c51d05
littlemesie <littlemesie@163.com>
2020-10-10 14:48:44 +0800

    dssm

---
+++ src/dssm/dssm_model.py
@@ -0,0 +1,14 @@
+import time
+import tensorflow as tf
+
+class Dssm:
+    def __init__(self, embedding_size=8, learning_rate=0.001, optimizer="adam", loss_type="logloss"):
+        self.embedding_size = embedding_size
+        self.learning_rate = learning_rate
+        self.optimizer = optimizer
+        self.loss_type = loss_type
+
+        self._init_graph()
+
+    def _init_graph(self):
+        self.graph = tf.Graph()
