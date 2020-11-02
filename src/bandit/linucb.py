35df4ac8ff55afc6cffc6ff4c65d3b1e13dc6377
littlemesie <littlemesie@163.com>
2020-11-02 19:02:38 +0800

    linucb

--- src/bandit/linucb.py
+++ src/bandit/linucb.py
@@ -1,4 +1,5 @@
 import numpy as np
+import io
 """
 参考https://github.com/KKeishiro/Yahoo_recommendation
 """
@@ -8,7 +9,7 @@ class LinUCB:
         self.r1 = 0.6
         self.r0 = -16
         self.d = 6  # dimension of user features
-        self.Aa = {} # Aa : collection of matrix to compute disjoint part for each article a, d*d
+        self.Aa = {}  # Aa : collection of matrix to compute disjoint part for each article a, d*d
         self.AaI = {}  # AaI : store the inverse of all Aa matrix
 
         self.ba = {}  # ba : collection of vectors to compute disjoin part, d*1
@@ -16,23 +17,19 @@ class LinUCB:
 
         self.a_max = 0
 
-
-
         self.x = None
         self.xT = None
 
-
-    def set_articles(self,art):
+    def set_articles(self, art):
         for key in art:
-            self.Aa[key] = np.identity(self.d) # 创建单位矩阵
-            self.ba[key] = np.zeros((self.d,1))
+            self.Aa[key] = np.identity(self.d)  # 创建单位矩阵
+            self.ba[key] = np.zeros((self.d, 1))
 
             self.AaI[key] = np.identity(self.d)
-            self.theta[key] = np.zeros((self.d,1))
+            # self.theta[key] = np.zeros((self.d, 1))
+            self.theta[key] = np.transpose([art[key]])
 
-
-
-    def update(self,reward):
+    def update(self, reward):
         if reward == -1:
             pass
         elif reward == 1 or reward == 0:
@@ -41,34 +38,59 @@ class LinUCB:
             else:
                 r = self.r0
 
-            self.Aa[self.a_max] += np.dot(self.x,self.xT)
+            self.Aa[self.a_max] += np.dot(self.x, self.xT)
             self.ba[self.a_max] += r * self.x
             self.AaI[self.a_max] = np.linalg.inv(self.Aa[self.a_max])
-            self.theta[self.a_max] = np.dot(self.AaI[self.a_max],self.ba[self.a_max])
+            self.theta[self.a_max] = np.dot(self.AaI[self.a_max], self.ba[self.a_max])
 
         else:
             # error
             pass
 
-
-    def recommend(self,timestamp,user_features,articles):
-        xaT = np.array([user_features]) # d * 1
+    def recommend(self, user_features, articles):
+        xaT = np.array([user_features])  # d * 1
         xa = np.transpose(xaT)
-
         AaI_tmp = np.array([self.AaI[article] for article in articles])
         theta_tmp = np.array([self.theta[article] for article in articles])
-        art_max = articles[np.argmax(np.dot(xaT,theta_tmp) + self.alpha * np.sqrt(np.dot(np.dot(xaT,AaI_tmp),xa)))]
+
+        art_max = articles[np.argmax(np.dot(xaT, theta_tmp) + self.alpha * np.sqrt(np.dot(np.dot(xaT, AaI_tmp), xa)))]
 
         self.x = xa
         self.xT = xaT
 
         self.a_max = art_max
+        self.update(reward=0)
+        print(art_max)
+        # return self.a_max
 
-        return self.a_max
 
+if __name__ == '__main__':
+    """"""
+    log_file = '../../data/brandit/webscope-logs.txt'
+    articles_file = '../../data/brandit/webscope-articles.txt'
+    model = LinUCB()
+    articles_np = np.loadtxt(articles_file)
 
-
-
+    articles_info = {}
+    articles = set()
+    for art in articles_np:
+        articles_info[int(art[0])] = [float(x) for x in art[1:]]
+        articles.add(int(art[0]))
+    user_features = []
+    with io.open(log_file, 'rb', buffering=1024*1024*512) as inf:
+        for line in inf:
+            arr = line.strip().split()
+            user_features.append([float(x) for x in arr[1:7]])
+    model.set_articles(articles_info)
+    # print(model.theta)
+    for i in range(10):
+        model.recommend(user_features[0], list(articles))
+        model.recommend(user_features[1], list(articles))
+    # print(model.a_max)
+    # model.recommend(user_features[1], list(articles))
+    # print(model.a_max)
+    # model.recommend(user_features[1], list(articles))
+    # print(model.a_max)
 
 
 
