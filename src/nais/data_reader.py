7eeda7f0f0ecf78b0afe68777eac1b30a8eabb17
littlemesie <littlemesie@163.com>
2021-01-05 19:14:19 +0800

    fism

---
+++ src/nais/data_reader.py
@@ -0,0 +1,124 @@
+import numpy as np
+import pandas as pd
+import scipy.sparse as sp
+import os
+
+DATA_DIR = '../../data/ml-100k/u.data'
+DATA_PATH = 'data/'
+COLUMN_NAMES = ['user', 'item']
+ITEM_CLIP = 300
+
+class Dataset:
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.load_data()
+
+
+    def map_index(self, item_set):
+
+        i = 0
+        items_map = {}
+        for item in item_set:
+            items_map[item] = i
+            i += 1
+        return items_map
+
+    def load_data(self):
+        full_data = pd.read_csv(DATA_DIR, sep='\t', header=None, names=COLUMN_NAMES, usecols=[0, 1],
+                                dtype={0: np.int32, 1: np.int32}, engine='python')
+
+        full_data.user = full_data['user'] - 1
+        user_set = set(full_data['user'].unique())
+        item_set = set(full_data['item'].unique())
+
+        self.user_size = len(user_set)
+        self.item_size = len(item_set)
+
+        # self.user_map = self.map_index(user_set)
+        # print(self.user_map)
+        # full_data['user'] = full_data['user'].map(lambda x: self.user_map[x])
+
+        self.item_map = self.map_index(item_set)
+        full_data['item'] = full_data['item'].map(lambda x: self.item_map[x])
+
+        index_item_set = set(full_data.item.unique())
+
+        # {user:[items]}
+        user_bought = {}
+        for i in range(len(full_data)):
+            u = full_data['user'][i]
+            t = full_data['item'][i]
+            if u not in user_bought:
+                user_bought[u] = []
+            user_bought[u].append(t)
+
+        # 没有购买的item
+        user_negative = {}
+        for key in user_bought:
+            user_negative[key] = list(index_item_set - set(user_bought[key]))
+
+        # 划分train-test
+        user_length = full_data.groupby('user').size().tolist()
+        split_train_test = []
+        for i in range(len(user_set)):
+            for _ in range(user_length[i] - 1):
+                split_train_test.append('train')
+            split_train_test.append('test')
+
+        full_data['split'] = split_train_test
+
+        train_data = full_data[full_data['split'] == 'train'].reset_index(drop=True)
+        test_data = full_data[full_data['split'] == 'test'].reset_index(drop=True)
+        del train_data['split']
+        del test_data['split']
+
+        labels = np.ones(len(train_data), dtype=np.int32)
+
+        train_features = train_data
+        self.train_labels = labels.tolist()
+
+        test_features = test_data
+
+        self.test_labels = test_data['item'].tolist()
+        train_data.sort_values("user", inplace=True)
+        # print(train_data.head())
+        # test_data.sort_values("user", inplace=True)
+
+        self.test_list, self.negative_list = self.get_test_list(test_data, user_negative)
+        self.train_list = self.get_train_list(train_data)
+        self.train_matrix = self.get_train_matrix(train_data)
+
+
+    def get_test_list(self, test_data, user_negative):
+        test_list = []
+        negative_list = []
+        for i in range(len(test_data)):
+            negative = user_negative[test_data['user'][i]]
+            test_list.append([test_data['user'][i], test_data['item'][i]])
+            negative_list.append(negative if len(negative) < 100 else negative[:100])
+        return test_list, negative_list
+
+    def get_train_list(self, train_data):
+        train_dict = {}
+        for i in range(len(train_data)):
+
+            user, item = int(train_data['user'][i]), int(train_data['item'][i])
+            train_dict.setdefault(user, [])
+            train_dict[user].append(item)
+
+        return list(train_dict.values())
+
+    def get_train_matrix(self, train_data):
+
+        mat = sp.dok_matrix((self.user_size+1, self.item_size+1), dtype=np.float32)
+        for i in range(len(train_data)):
+            user, item = int(train_data['user'][i]), int(train_data['item'][i])
+            mat[user, item] = 1.0
+        return mat
+
+if __name__ == '__main__':
+    """"""
+    d = Dataset()
---
+++ src/nais/fism.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+class FISM:
+
+    def __init__(self, user_size, item_size, lr=0.01, embed_size=16,
+                 alpha=0, verbose=1, regs=[1e-7,1e-7], batch_choice="user",
+                 train_loss=1):
+        # self.user_size = user_size
+        self.item_size = item_size
+
+        self.learning_rate = lr
+        self.embedding_size = embed_size
+        self.alpha = alpha
+        self.verbose = verbose
+        self.lambda_bilinear = regs[0]
+        self.gamma_bilinear = regs[1]
+        self.batch_choice = batch_choice
+        self.train_loss = train_loss
+
+        self.build_graph()
+
+    def _create_placeholders(self):
+        with tf.name_scope("input"):
+            self.user_input = tf.placeholder(tf.int32, shape=[None, None])	#the index of users
+            self.num_idx = tf.placeholder(tf.float32, shape=[None, 1])	#the number of items rated by users
+            self.item_input = tf.placeholder(tf.int32, shape=[None, 1])	  #the index of items
+            self.labels = tf.placeholder(tf.float32, shape=[None, 1])	#the ground truth
+
+    def _create_variables(self):
+        with tf.name_scope("embedding"):  # The embedding initialization is unknown now
+            self.c1 = tf.Variable(tf.truncated_normal(shape=[self.item_size, self.embedding_size], mean=0.0, stddev=0.01), #why [0, 3707)?
+                                                 name='c1', dtype=tf.float32)
+            self.c2 = tf.constant(0.0, tf.float32, [1, self.embedding_size], name='c2' )
+            self.embedding_Q_ = tf.concat([self.c1,self.c2], 0, name='embedding_Q_')
+            self.embedding_Q = tf.Variable(tf.truncated_normal(shape=[self.item_size, self.embedding_size], mean=0.0, stddev=0.01),
+                                                                name='embedding_Q', dtype=tf.float32)
+            self.bias = tf.Variable(tf.zeros(self.item_size),name='bias')
+
+    def _create_inference(self):
+        with tf.name_scope("inference"):
+            self.embedding_p = tf.reduce_sum(tf.nn.embedding_lookup(self.embedding_Q_, self.user_input), 1)
+            self.embedding_q = tf.reduce_sum(tf.nn.embedding_lookup(self.embedding_Q, self.item_input), 1)
+            self.bias_i = tf.nn.embedding_lookup(self.bias, self.item_input)
+            self.coeff = tf.pow(self.num_idx, -tf.constant(self.alpha, tf.float32, [1]))
+            self.output = tf.sigmoid(self.coeff * tf.expand_dims(tf.reduce_sum(self.embedding_p*self.embedding_q, 1),1) + self.bias_i)
+
+    def _create_loss(self):
+        with tf.name_scope("loss"):
+            self.loss = tf.losses.log_loss(self.labels, self.output) + \
+                        self.lambda_bilinear*tf.reduce_sum(tf.square(self.embedding_Q)) + self.gamma_bilinear*tf.reduce_sum(tf.square(self.embedding_Q_))
+            # self.loss = tf.nn.l2_loss(self.labels - self.output) + \
+            #             self.lambda_bilinear * tf.reduce_sum(tf.square(self.embedding_Q)) + self.gamma_bilinear * tf.reduce_sum(tf.square(self.embedding_Q_))
+
+    def _create_optimizer(self):
+        with tf.name_scope("optimizer"):
+            self.optimizer = tf.train.AdagradOptimizer(learning_rate=self.learning_rate, initial_accumulator_value=1e-8).minimize(self.loss)
+
+    def build_graph(self):
+        self._create_placeholders()
+        self._create_variables()
+        self._create_inference()
+        self._create_loss()
+        self._create_optimizer()
