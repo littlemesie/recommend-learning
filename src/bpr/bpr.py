d099a891feae3e612cc8128ede55c0e334392176
littlemesie <littlemesie@163.com>
2021-03-22 17:45:43 +0800

    update

--- src/bpr/bpr.py
+++ src/bpr/bpr.py
@@ -9,20 +9,20 @@ def load_data():
     user_ratings = defaultdict(set)
     max_u_id = -1
     max_i_id = -1
-    with open('data/u.data','r') as f:
+    with open('../../data/ml-100k/u.data', 'r') as f:
         for line in f.readlines():
-            u,i,_,_ = line.split("\t")
+            u, i, _, _ = line.split("\t")
             u = int(u)
             i = int(i)
             user_ratings[u].add(i)
-            max_u_id = max(u,max_u_id)
-            max_i_id = max(i,max_i_id)
+            max_u_id = max(u, max_u_id)
+            max_i_id = max(i, max_i_id)
 
 
-    print("max_u_id:",max_u_id)
-    print("max_i_idL",max_i_id)
+    print("max_u_id:", max_u_id)
+    print("max_i_idL", max_i_id)
 
-    return max_u_id,max_i_id,user_ratings
+    return max_u_id, max_i_id, user_ratings
 
 def generate_test(user_ratings):
     """
@@ -30,12 +30,12 @@ def generate_test(user_ratings):
     后面构造训练集和测试集需要用到。
     """
     user_test = dict()
-    for u,i_list in user_ratings.items():
-        user_test[u] = random.sample(user_ratings[u],1)[0]
+    for u, i_list in user_ratings.items():
+        user_test[u] = random.sample(user_ratings[u], 1)[0]
     return user_test
 
 
-def generate_train_batch(user_ratings,user_ratings_test,item_count,batch_size=512):
+def generate_train_batch(user_ratings, user_ratings_test, item_count, batch_size=512):
     """
     构造训练用的三元组
     对于随机抽出的用户u，i可以从user_ratings随机抽出，而j也是从总的电影集中随机抽出，当然j必须保证(u,j)不在user_ratings中
@@ -43,16 +43,16 @@ def generate_train_batch(user_ratings,user_ratings_test,item_count,batch_size=51
     """
     t = []
     for b in range(batch_size):
-        u = random.sample(user_ratings.keys(),1)[0]
-        i = random.sample(user_ratings[u],1)[0]
-        while i==user_ratings_test[u]:
-            i = random.sample(user_ratings[u],1)[0]
+        u = random.sample(user_ratings.keys(), 1)[0]
+        i = random.sample(user_ratings[u], 1)[0]
+        while i == user_ratings_test[u]:
+            i = random.sample(user_ratings[u], 1)[0]
 
-        j = random.randint(1,item_count)
+        j = random.randint(1, item_count)
         while j in user_ratings[u]:
-            j = random.randint(1,item_count)
+            j = random.randint(1, item_count)
 
-        t.append([u,i,j])
+        t.append([u, i, j])
 
     return np.asarray(t)
 
@@ -71,10 +71,10 @@ def generate_test_batch(user_ratings,user_ratings_test,item_count):
         yield np.asarray(t)
 
 
-def bpr_mf(user_count,item_count,hidden_dim):
-    u = tf.placeholder(tf.int32,[None])
-    i = tf.placeholder(tf.int32,[None])
-    j = tf.placeholder(tf.int32,[None])
+def bpr_mf(user_count, item_count, hidden_dim):
+    u = tf.placeholder(tf.int32, [None])
+    i = tf.placeholder(tf.int32, [None])
+    j = tf.placeholder(tf.int32, [None])
 
     user_emb_w = tf.get_variable("user_emb_w", [user_count + 1, hidden_dim],
                                  initializer=tf.random_normal_initializer(0, 0.1))
@@ -86,9 +86,9 @@ def bpr_mf(user_count,item_count,hidden_dim):
     j_emb = tf.nn.embedding_lookup(item_emb_w, j)
 
 
-    x = tf.reduce_sum(tf.multiply(u_emb,(i_emb-j_emb)),1,keep_dims=True)
+    x = tf.reduce_sum(tf.multiply(u_emb, (i_emb - j_emb)), 1, keep_dims=True)
 
-    mf_auc = tf.reduce_mean(tf.to_float(x>0))
+    mf_auc = tf.reduce_mean(tf.to_float(x > 0))
 
     l2_norm = tf.add_n([
         tf.reduce_sum(tf.multiply(u_emb, u_emb)),
@@ -100,27 +100,28 @@ def bpr_mf(user_count,item_count,hidden_dim):
     bprloss = regulation_rate * l2_norm - tf.reduce_mean(tf.log(tf.sigmoid(x)))
 
     train_op = tf.train.GradientDescentOptimizer(0.01).minimize(bprloss)
+
     return u, i, j, mf_auc, bprloss, train_op
 
 
-user_count,item_count,user_ratings = load_data()
+user_count, item_count, user_ratings = load_data()
 user_ratings_test = generate_test(user_ratings)
 
 with tf.Session() as sess:
-    u,i,j,mf_auc,bprloss,train_op = bpr_mf(user_count,item_count,20)
+    u, i, j, mf_auc, bprloss, train_op = bpr_mf(user_count, item_count,20)
     sess.run(tf.global_variables_initializer())
 
-    for epoch in range(1,4):
+    for epoch in range(1, 4):
         _batch_bprloss = 0
-        for k in range(1,5000):
-            uij = generate_train_batch(user_ratings,user_ratings_test,item_count)
-            _bprloss,_train_op = sess.run([bprloss,train_op],
-                                          feed_dict={u:uij[:,0],i:uij[:,1],j:uij[:,2]})
+        for k in range(1, 5000):
+            uij = generate_train_batch(user_ratings, user_ratings_test, item_count)
+            _bprloss,_train_op = sess.run([bprloss, train_op],
+                                          feed_dict={u: uij[:, 0], i: uij[:, 1], j: uij[:, 2]})
 
             _batch_bprloss += _bprloss
 
-        print("epoch:",epoch)
-        print("bpr_loss:",_batch_bprloss / k)
+        print("epoch:", epoch)
+        print("bpr_loss:", _batch_bprloss / k)
         print("_train_op")
 
         user_count = 0
@@ -141,16 +142,16 @@ with tf.Session() as sess:
         print("Shape: ", v.shape)
         print(v)
 
-#  0号用户对这个用户对所有电影的预测评分
+# 0号用户对这个用户对所有电影的预测评分
 session1 = tf.Session()
 u1_dim = tf.expand_dims(values[0][0], 0)
-u1_all = tf.matmul(u1_dim, values[1],transpose_b=True)
+u1_all = tf.matmul(u1_dim, values[1], transpose_b=True)
 result_1 = session1.run(u1_all)
-print (result_1)
+print(result_1)
 
 print("以下是给用户0的推荐：")
 p = np.squeeze(result_1)
 p[np.argsort(p)[:-5]] = 0
 for index in range(len(p)):
     if p[index] != 0:
-        print (index, p[index])
+        print(index, p[index])
